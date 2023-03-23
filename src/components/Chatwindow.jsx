f5320ed2e33f00b98abeac6362240fad2b865d93
Naman Rajput <namanrajput801@gmail.com>
2023-03-23 19:40:39 +0530

    changes in screen

--- src/components/Chatwindow.jsx
+++ src/components/Chatwindow.jsx
@@ -4,14 +4,19 @@ import Profile from "./Profile";
 
 const Chatwindow = () => {
   return (
-    <div className="flex h-20 px-5 bg-blue-500 w-[80vw] items-center justify-between ">
-      <img src="https://img.icons8.com/external-creatype-outline-colourcreatype/64/null/external-user-interface-a1-creatype-outline-colourcreatype.png" />
-      <div className="closeIcon">
-        <img src="https://img.icons8.com/ios-filled/50/null/multiply.png" />
+    <>
+      <div className="fixed left-[20vw] h-screen ">
+        <div className="flex h-20 px-5 bg-blue-500 w-[80vw] items-center justify-between ">
+          <img src="https://img.icons8.com/external-creatype-outline-colourcreatype/64/null/external-user-interface-a1-creatype-outline-colourcreatype.png" />
+          <div className="closeIcon">
+            <img src="https://img.icons8.com/ios-filled/50/null/multiply.png" />
+          </div>
+        </div>
+        <div />
+
+        <Messagebar />
       </div>
-      <div />
-      <Messagebar />
-    </div>
+    </>
   );
 };
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,11 +3,11 @@ import profileIcon from "../assets/profile-icon-png-898.png";
 
 const Profile = (props) => {
   return (
-    <div className="relative z-10 flex flex-row items-center justify-center p-6 border-2 border-t-0 cursor-pointer">
-      <div className="image">
-        <img src={profileIcon} alt="" className="h-[3rem] w-[3rem]" />
+    <div className="relative z-10 flex items-center border-b-2 border-r-2 cursor-pointer justify-evenly">
+      <div className="image ">
+        <img src={profileIcon} alt="" className="h-20" />
       </div>
-      <div className="px-24 font-bold username">{props.name}</div>
+      <div className="font-bold username">{props.name}</div>
     </div>
   );
 };
